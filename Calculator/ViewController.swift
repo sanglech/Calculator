97a895e9d6aeb9f0f32194ed0ba92dd99c548e2a
Christian Sangle <christian.sangle@gmail.com>
2015-06-24 18:25:33 -0400

    Broke all of assignment 1 extra stuff. Main Calculator works, using MVC design methodology.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,12 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
-    @IBOutlet weak var history: UILabel!
-    
     var userIsInMiddleOfTypingANumber=false
-    var historyStack = Array<String>()
-    var decimalAlreadyUsed=false
-    var decCount=0
+    var brain=CalculatorBrain()
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -29,21 +25,6 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func appendDecimal(sender: UIButton) {
-        let decimal=sender.currentTitle!
-        display.text="0"
-        if(decCount==0){
-            display.text=display.text!+decimal
-            userIsInMiddleOfTypingANumber=true
-            decCount++
-        }
-        else{
-            decCount++
-        }
-    }
-    
-    
-    var opperandStack = Array<Double>()
     @IBAction func operate(sender: UIButton) {
         let operation=sender.currentTitle!
         if userIsInMiddleOfTypingANumber{
@@ -51,68 +32,29 @@ class ViewController: UIViewController {
         }
         
         /* Swift able to infer because we are passing functions it knows what it should be returning and the type of the arguments (because perform operations) that we dont need to specifiy it here. Just need to write functions in line here  */
-        historyStack.append(operation)
-        switch operation{
-        case "×": performOperation({$0 * $1} )
-        case "÷": performOperation({op1,op2 in return op2 / op1})
-        case "+": performOperation({op1, op2 in op1 + op2})
-        case "−": performOperation { $1 - $0 }
-        case "√": performOperation1{ sqrt($0) }
-        case "sin": performOperation1{ sin($0)}
-        case "cos": performOperation1{ cos($0) }
-        case "±": performOperation1{ $0 * -1}
-        default: break
+        if let operation=sender.currentTitle{
+            if let result=brain.performOperation(operation){
+                displayValue=result
+            }
+            else{
+                displayValue=0
+            }
         }
     }
     
-    func performOperation(operation : (Double, Double)->Double){
-        if(opperandStack.count>=2){
-            displayValue = operation(opperandStack.removeLast(),opperandStack.removeLast())
-            var operationOccuring=historyStack.removeLast()
-            historyWith2Args(historyStack.removeLast(), arg2: historyStack.removeLast(), arg3: operationOccuring)
-            enter()
-            let stringDispVal=historyStack.last!
-            display.text="= \(stringDispVal)"
-        }
-    }
-    
-    func historyWith2Args(arg1: String,arg2:String,arg3:String){
-         history.text=arg2 + arg3+arg1
-    }
-    
-    func historyWith1Args(arg1: String,arg2:String){
-        history.text=arg1 + arg2
-    }
-    
-    func performOperation1(operation :Double -> Double){
-        if(opperandStack.count>=1){
-            displayValue = operation(opperandStack.removeLast())
-            historyWith1Args(historyStack.removeLast(),arg2:historyStack.removeLast())
-            enter()
-            let stringDispVal=historyStack.last!
-            display.text="= \(stringDispVal)"
-        }
-    }
     
     @IBAction func clear() {
-        opperandStack.removeAll()
-        historyStack.removeAll()
         display.text!="0"
-        history.text!=""
     }
     
     @IBAction func enter() {
         userIsInMiddleOfTypingANumber=false
-        decCount=0
-        if(display.text!=="π"){
-            opperandStack.append(M_PI)
+        if let result=brain.pushOpperand(displayValue){
+            displayValue=result
         }
         else{
-            opperandStack.append(displayValue)
+            displayValue=0
         }
-        historyStack.append(display.text!)
-        println("opperand stack= \(opperandStack)")
-        println("history stack= \(historyStack)")
     }
     
     var displayValue:Double {
--- CalculatorBrain.swift
+++ CalculatorBrain.swift
@@ -10,14 +10,82 @@ import Foundation
 
 class CalculatorBrain{
     
-    enum Op{
+    private enum Op : Printable{
         case UnaryOperation (String,Double->Double)
         case BinaryOperation(String, (Double,Double)->Double)
         case Operand (Double)
+        var description: String {
+        get {
+            switch self{
+            case .Operand(let operand):
+                return ("\(operand)")
+            case .UnaryOperation(let symbol, _):
+                return symbol
+            case .BinaryOperation(let symbol, _):
+                return symbol
+            }
+        }
     }
-    var opStack = [Op]()
     
-    func pushOpperand(operand:Double){
+    }
+    private var opStack = [Op]()
+    
+    /*Declare a dictionary same as Dictionary<String,Op>() */
+    
+    private var knownOps = [String:Op]()
+    
+    init(){
+        func learnOp(op: Op){
+            knownOps[op.description]=op
+        }
+        learnOp(Op.BinaryOperation("×", *))
+        learnOp(Op.BinaryOperation("÷") {$1 / $0})
+        learnOp(Op.BinaryOperation("+", +))
+        learnOp(Op.BinaryOperation("−") {$1 - $0})
+        learnOp(Op.UnaryOperation("√", sqrt))
+        
+    }
+    
+   private func evaluate(ops:[Op])->(result:Double?,remainingOps:[Op]){
+        if !ops.isEmpty{
+            var remaingOps=ops
+            let op = remaingOps.removeLast()
+            switch op{
+            case .Operand(let operand):
+                return(operand,remaingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation=evaluate(remaingOps)
+                if let operand=operandEvaluation.result{
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation=evaluate(remaingOps)
+                if let operand1=op1Evaluation.result{
+                    let op2Evaluation=evaluate(op1Evaluation.remainingOps)
+                    if let operand2=op2Evaluation.result{
+                        return(operation(operand1,operand2),op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        return (nil, ops)
+    }
+    //use OPtional/return qhwn want to return null/nill
+    func evaluate() ->Double? {
+    let (result, remainder)=evaluate(opStack)
+        println("\(opStack) = \(result) with \(remainder) left over")
+        return result
+    }
+    
+    func pushOpperand(operand:Double) ->Double?{
         opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func performOperation(symbol:String) -> Double?{
+        if let operation=knownOps[symbol] {
+            opStack.append(operation)
+        }
+        return evaluate()
     }
 }
